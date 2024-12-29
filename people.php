fa3e0449e94db9f742cbdce4d16f210849f945ec
donguyenphu <donguyenphu022008@gmail.com>
2024-12-29 14:29:24 +0700

    update people

--- people.php
+++ people.php
@@ -9,16 +9,16 @@
     <div class="peopleContact">
         <div class="peopleInformationWrapper">
             <div class="titleIntroductionWrapper">
-                <h1 class="titleIntroductionMain">
+                <h2 class="titleIntroductionMain">
                     Travel
-                </h1>
+                </h2>
             </div>
             <div class="peopleWrapper">
                 <div id="HaiPhongPeopleDiv1">
                     <div class="peopleIntroductionWrapper">
-                        <h1 class="peopleIntroductionMain">
+                        <p class="peopleIntroductionMain">
                             Hai Phong people are famous for heartwarming welcome, hard-working and also daring to do what they talked.
-                        </h1>
+                        </p>
                     </div>
                     <div class="peopleImageWrapper" id="VangToiHP">
                         <img src="./image/Vang-toi-HP.jpg" class="peopleImageMain">
@@ -26,9 +26,9 @@
                 </div>
                 <div id="HaiPhongPeopleDiv2">
                     <div class="peopleIntroductionWrapper">
-                        <h1 class="peopleIntroductionMain">
-                            When foreign visitors come to The Harbour City, they will receive warm welcome form our people, and also eager guides to every corners of Hai Phong City.
-                        </h1>
+                        <p class="peopleIntroductionMain">
+                            Foreign visitors will receive warm welcome form our people, and also eager guides to every corners of The Habour City.
+                        </p>
                     </div>
                     <div class="peopleImageWrapper" id="foreignTouristHaiPhong">
                         <img src="./image/foreign_tourists_Hai_Phong.jpg" class="peopleImageMain">
@@ -36,23 +36,23 @@
                 </div>
             </div>
             <div class="titleIntroductionWrapper">
-                <h1 class="titleIntroductionMain">
+                <h2 class="titleIntroductionMain">
                     Song
-                </h1>
+                </h2>
             </div>
             <div class="songWrapper">
                 <div class="songIntroductionWrapper">
-                    <h1 class="songIntroductionMain">
+                    <p class="songIntroductionMain">
                         When coming to Hai Phong city, everyone will remember the song Tôi người Hải Phòng , the song has an overall view of Hai Phong, as known as "Flamboyant City" , from history to people and so on.
-                    </h1>
+                    </p>
                 </div>
                 <div class="songLinkWrapper">
                     <img src="./image/ToiNguoiHaiPhong.jpg" class="songLinkMain">
                 </div>
                 <div class="songIntroductionWrapper">
-                    <h1 class="songIntroductionMain">
+                    <p class="songIntroductionMain">
                         The song was finished in April 20,1972 , by artist Xuan Binh. The song was opened to Voice of Vietnam radio waves. The song was considered as a memory, a big love to The Harbour City, After 3 years, the author of this song sang to people in Hai Phong.
-                    </h1>
+                    </p>
                 </div>
                 <div class="songLinkWrapper">
                     <img src="./image/BucTuongHaiPhong.jpg" class="songLinkMain">
@@ -62,9 +62,9 @@
                         <a href="https://www.youtube.com/watch?v=k49gP1FX5eo" class="seeMoreMain">
                             <i class="fa-solid fa-angles-down" id="ToiNguoiHaiPhong"></i>
                             <div class="seeMoreIntroductionWrapper">
-                                <h1 class="seeMoreIntroductionMain">
+                                <h3 class="seeMoreIntroductionMain">
                                     See more about our song here
-                                </h1>
+                                </h3>
                             </div>
                         </a>
                     </div>
