afeaed28fe65bc6429a950c50cc8de3a117a274a
donguyenphu <donguyenphu022008@gmail.com>
2025-01-03 06:01:01 +0700

    update cuisine image, content

--- cuisine.php
+++ cuisine.php
@@ -19,37 +19,39 @@
                 </a>
             </div>
             <div class="CrabNoodleSoupWrapper">
-                <div class="imgCNSwrapper">
-                    <img src="./image/CrabNoodleSoup.jpg" class="imgCNSmain">
+                <div class="imageAndContentCuisineWrapper">
+                    <div class="imgCNSwrapper">
+                        <img src="./image/CrabNoodleSoup.jpg" class="imgCNSmain">
+                    </div>
+                    <div class="foodDetailWrapper">
+                        <p class="foodDetailMain">
+                            Hai Phong crab noodle soup brings a rich, fatty but not greasy feeling, combined with the fresh taste of raw vegetables, the soft chewiness of rice noodles and the strong taste of the sea from crab and shrimp. A memorable dish!
+                        </p>
+                    </div> 
                 </div>
-                <div class="foodDetailWrapper">
-                    <h1 class="foodDetailMain">
-                        Hai Phong crab noodle soup brings a rich, fatty but not greasy feeling, combined with the fresh taste of raw vegetables, the soft chewiness of rice noodles and the strong taste of the sea from crab and shrimp. A memorable dish!
-                    </h1>
-                </div> 
                 <div class="locationWrapper">
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29816.26248046438!2d106.63760991083983!3d20.9109996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7bdf2eb4a9c3%3A0xfe9ffcd7975fcf75!2zQsO6biBDw6EgQ2F5IFRo4bqxbmcgQuG7nW0!5e0!3m2!1svi!2s!4v1733576521908!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Bún cá cay thằng Bờm
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59632.16541502954!2d106.63271074308481!3d20.911903722449452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7b9d0e04141b%3A0xbb5b8e4d8fdb6a35!2zQsO6biBDw6EgQ2F5IELDoCBCw6lv!5e0!3m2!1svi!2s!4v1733576624898!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Bún Cá Cay Bà Béo
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3727.010215201154!2d106.67133587417362!3d20.91190868070834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7b9db04c5223%3A0x93956b9366557399!2zQsO6biBDw6EgQ2F5IEPhu5VuZyBD4bqjbmcgxJDhurdjIFPhuqNuIEjhuqNpIFBow7JuZw!5e0!3m2!1svi!2s!4v1733576593298!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Bún Cá Cay Cổng Cảng
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                 </div>  
@@ -87,37 +89,41 @@
                 </a>
             </div>
             <div class="CoconutIceCreamWrapper">
-                <div class="imgCNSwrapper">
-                    <img src="./image/CoconutIceCream.jpg" class="imgCNSmain">
-                </div>
-                <div class="foodDetailWrapper">
-                    <h1 class="foodDetailMain">
-                        This dish is perfect for hot summer days or when you want to enjoy a unique, easy-to-eat dessert.The cool, crispy jelly layer, soft, chewy coconut meat, mixed with cool coconut milk, creates a refreshing feeling from the first bite.
-                    </h1>
+                <div class="imageAndContentCuisineWrapper">
+                    <div class="imageAndContentCuisineWrapper">
+                        <div class="imgCNSwrapper">
+                            <img src="./image/CoconutIceCream.jpg" class="imgCNSmain">
+                        </div>
+                        <div class="foodDetailWrapper">
+                            <p class="foodDetailMain">
+                                This dish is perfect for hot summer days or when you want to enjoy a unique, easy-to-eat dessert.The cool, crispy jelly layer, soft, chewy coconut meat, mixed with cool coconut milk, creates a refreshing feeling from the first bite.
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div class="locationWrapper">
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.645685832622!2d106.68879727417136!3d20.846009380757973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7b30df1e4d97%3A0xd306586f1081e48!2zROG7qkEgROG6pk0gSEnhu4BOIELDiU8!5e0!3m2!1svi!2s!4v1733569276357!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Dừa dầm Hiền Béo
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.607314647107!2d106.68840167417149!3d20.84755778075676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7a90f4c0d0ef%3A0x66fe01710b18aa4d!2zROG7q2EgROG6p20gQ8O0IFR1eeG6v24tIENhZmUgQ-G7kXQgROG7q2EgSOG6o2kgUGjDsm5n!5e0!3m2!1svi!2s!4v1733569330007!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Dừa dầm cô Tuyến - Cafe cốt dừa Hải Phòng
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.639877297973!2d106.68909347417132!3d20.846243780757888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7ba0015a8103%3A0xdd5fa0b6121edc02!2zROG7q2EgROG6p20gQ8O0IFZpbmg!5e0!3m2!1svi!2s!4v1733569366514!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Dừa dầm cô Vinh
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                 </div> 
@@ -179,37 +185,39 @@
                 </a>
             </div>
             <div class="SpicyBreadWrapper">
-                <div class="imgCNSwrapper">
-                    <img src="./image/Banh_my_que.jpg" class="imgCNSmain">
-                </div>
-                <div class="foodDetailWrapper">
-                    <h1 class="foodDetailMain">
-                        Hai Phong spicy bread, also known as baguette, is a famous street food specialty of Hai Phong. With its compact shape, crispy crust, fragrant pate filling, combined with the characteristic spicy taste of chili sauce, this dish has become a culinary symbol of the port city.
-                    </h1>
+                <div class="imageAndContentCuisineWrapper">
+                    <div class="imgCNSwrapper">
+                        <img src="./image/Banh_my_que.jpg" class="imgCNSmain">
+                    </div>
+                    <div class="foodDetailWrapper">
+                        <p class="foodDetailMain">
+                            Hai Phong spicy bread, also known as baguette, is a famous street food specialty of Hai Phong. With its compact shape, crispy crust, fragrant pate filling, combined with the characteristic spicy taste of chili sauce, this dish has become a culinary symbol of the port city.
+                        </p>
+                    </div>
                 </div>
                 <div class="locationWrapper">
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.3303190467614!2d106.69045357417187!3d20.85873218074844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7aec7c01572d%3A0x813d03397de354d1!2zQsOhbmggTeG7syBDYXkgQsOgIEdpw6AgTMOqIEzhu6NpIEjhuqNpIFBow7JuZw!5e0!3m2!1svi!2s!4v1733568477866!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Bánh mì cay bà già Lê Lợi Hải Phòng
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.599582588019!2d106.68278047417144!3d20.84786978075662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7a8fac5efec7%3A0x11debee10f6ecd39!2zVGnhu4dtIELDoW5oIE3DrCBDYXkgw5RuZyBDdcO0bmc!5e0!3m2!1svi!2s!4v1733568538562!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"  class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Tiệm bánh mì cay ông Cuông
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.596446363947!2d106.68288302417142!3d20.847996330756484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7bcd57a4cb41%3A0x684ce7b8397c8167!2zQsOhbmggbcOsIGNheSBI4bqjaSBQaMOybmcgLSBLaMOhbmggTuG6oXA!5e0!3m2!1svi!2s!4v1733568585676!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"  class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Bánh mì cay Hải Phòng - Khánh Nạp
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                 </div>
@@ -259,37 +267,39 @@
                 </a>
             </div>
             <div class="StirFriedFishPriceWrapper">
-                <div class="imgCNSwrapper">
-                    <img src="./image/giaBeXao.jpeg" class="imgCNSmain">
-                </div>
-                <div class="foodDetailWrapper">
-                    <h1 class="foodDetailMain">
-                        Stir-fried bean sprouts with Hai Phong is a rustic but extremely special dish of the port city, famous for its rich flavor and unique preparation method. Bean sprouts are a small seafood, shaped like bean sprouts, often stir-fried with spices and herbs to create a delicious and unique dish.
-                    </h1>
+                <div class="imageAndContentCuisineWrapper">
+                    <div class="imgCNSwrapper">
+                        <img src="./image/giaBeXao.jpeg" class="imgCNSmain">
+                    </div>
+                    <div class="foodDetailWrapper">
+                        <p class="foodDetailMain">
+                            Stir-fried bean sprouts with Hai Phong is a rustic but extremely special dish of the port city, famous for its rich flavor and unique preparation method. Bean sprouts are a small seafood, shaped like bean sprouts, often stir-fried with spices and herbs to create a delicious and unique dish.
+                        </p>
+                    </div>
                 </div>
                 <div class="locationWrapper">
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.4841982122052!2d106.6863720741717!3d20.852525180753084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7b12548392fb%3A0x9e1ed33104652852!2zR2nDoSBi4buDIGNo4bujIENodSBWxINuIEFu!5e0!3m2!1svi!2s!4v1733576908943!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Giá bể chợ Chu Văn An
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.557874648664!2d106.68248877417157!3d20.8495526807553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7a8e8aac1ec3%3A0xb1f20750f9892a23!2zQsOgIE1pbmgtR2nDoSBi4buD!5e0!3m2!1svi!2s!4v1733576990085!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Bà Minh-Giá bể
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                     <div class="locationDetailWrapper">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3728.548722772593!2d106.66639357073532!3d20.849921938221744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a7b0034579b4b%3A0x797a0871c07a559b!2zR2nDoSBC4buDIFjDoG8sIEfhu49p!5e0!3m2!1svi!2s!4v1733577074253!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="BMC"></iframe>
                         <div class="describeLocationWrapper">
-                            <h1 class="describeLocationMain">
+                            <h2 class="describeLocationMain">
                                 Giá Bể Xào, Gỏi
-                            </h1>
+                            </h2>
                         </div>
                     </div>
                 </div>
