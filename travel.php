b94f6d09e07648e1455118d1273c9874d156da66
donguyenphu <donguyenphu022008@gmail.com>
2025-01-04 05:56:05 +0700

    update Cat Ba cuisines images

--- travel.php
+++ travel.php
@@ -44,16 +44,16 @@
                 </div>
                 <div id="CuisinesDoSonImageIWrapper">
                     <div class="OnlyImageWrapper">
-                        <img src="./image/NomSuaDoSon.jpg" class="OnlyImageMain">
+                        <a href="https://cookpad.com/vn/cong-thuc/14151698-n%E1%BB%99m-s%E1%BB%A9a-d%E1%BB%93-s%C6%A1n-h%E1%BA%A3i-phongs"><img src="./image/NomSuaDoSon.jpg" class="OnlyImageMain"></a>
                     </div>
                     <div class="OnlyImageWrapper">
-                        <img src="./image/LauCuaDongNemCuaBe.jpg" class="OnlyImageMain">
+                        <a href="https://www.youtube.com/watch?v=jFZ16SZYRTs"><img src="./image/LauCuaDongNemCuaBe.jpg" class="OnlyImageMain"></a>
                     </div>
                     <div class="OnlyImageWrapper">
-                        <img src="./image/CaMoiMotNangDoSon.jpeg" class="OnlyImageMain">
+                        <a href="https://www.vitot.vn/blogs/cong-thuc-nau-ngon/7-cach-lam-mon-ca-moi-1-mot-nang?srsltid=AfmBOopJk8o-j36zgqFlKLfcQzmx4ZDkLyE4pNsBs-nu3U6MPbDQoBiW"><img src="./image/CaMoiMotNangDoSon.jpeg" class="OnlyImageMain"></a>
                     </div>  
                     <div class="OnlyImageWrapper">
-                        <img src="./image/HaiSanDoSon.jpg" class="OnlyImageMain">
+                        <a href="https://xaydungso.vn/blog/biet-cach-che-bien-hai-san-do-son-tro-thanh-mon-an-ngon-vi-cb.html"><img src="./image/HaiSanDoSon.jpg" class="OnlyImageMain"></a>
                     </div>
                 </div>
                 <div class="infoCuisineDoSonWrapper">
@@ -97,6 +97,46 @@
                         <h3 class="locationTitleMain">
                             Cuisines:
                         </h3>
+                    </div>
+                </div>
+                <div id="CuisinesDoSonImageIWrapper">
+                    <div class="OnlyImageWrapper">
+                        <a href="https://www.youtube.com/watch?v=uRUWJXmEhJc"><img src="./image/horseShoeCrab.jpg" class="OnlyImageMain"></a>
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://www.dienmayxanh.com/vao-bep/cach-lam-ca-song-ca-mu-hap-gung-hanh-ngon-ngot-chieu-dai-gia-08854"><img src="./image/CatBaGrouper.jpg" class="OnlyImageMain"></a>
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://chefdzung.com.vn/cach-che-bien-tu-hai-hap-sa.html"><img src="./image/CatBaGeoduck.jpg" class="OnlyImageMain"></a>
+                    </div>  
+                    <div class="OnlyImageWrapper">
+                        <a href="https://haisan.online/be-be-sot-me-139782.html"></a><img src="./image/CatBaMantisShrimp.jpg" class="OnlyImageMain">
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://kingfoodmart.com/bai-viet/2-cach-che-bien-tom-hum-ngon-tuyet-dinh?srsltid=AfmBOopNtXfSZ7nFF87SHxr_wG6_B6vTHzOZcMq02rLp3lizq9gpWmlb"></a><img src="./image/CatBaLobster.jpg" class="OnlyImageMain">
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://www.dienmayxanh.com/vao-bep/cach-lam-bun-tom-hai-phong-chua-chua-ngot-ngot-la-mieng-thom-08299"><img src="./image/vermicelliWithShrimp.jpg" class="OnlyImageMain"></a>
+                    </div>
+                </div>
+                <div class="infoCuisineDoSonWrapper">
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba horseshoe crab</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba grouper</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba geoduck</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba mantis shrimp</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba lobster</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba vermicelli with shrimp</h3>
                     </div>
                 </div>
             </div>
