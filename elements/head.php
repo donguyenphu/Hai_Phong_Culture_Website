e2d589f7dbbb71252c3a6c2c26728f2d444b72fe
donguyenphu <donguyenphu022008@gmail.com>
2025-01-06 22:04:44 +0700

    update Lan Ha Bay information

--- elements/head.php
+++ elements/head.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- FONTAWESOME START -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!-- FONTAWESOME END -->
--- travel.php
+++ travel.php
@@ -14,6 +14,7 @@
         </div>
         <div class="peopleInformationWrapper">
             <!-- INFO START -->
+            <!-- Do Son Beach -->
             <div class="allInfoTravelWrapper">
                 <div class="titleIntroductionWrapper">
                     <h2 class="titleFoodMain">
@@ -71,6 +72,7 @@
                     </div>
                 </div>
             </div>
+            <!-- Cat Ba Island -->
             <div class="allInfoTravelWrapper">
                 <div class="titleIntroductionWrapper">
                     <h2 class="titleFoodMain">
@@ -140,12 +142,129 @@
                     </div>
                 </div>
             </div>
+            <!-- Lan Ha Bay -->
             <div class="allInfoTravelWrapper">
-
+                <div class="titleIntroductionWrapper">
+                    <h2 class="titleFoodMain">
+                        Lan Ha Bay
+                    </h2>
+                </div> 
+                <div id="DoSonBeachWrapper">
+                    <div class="locationTitleWrapper">
+                        <h3 class="locationTitleMain">
+                            Location: 
+                        </h3>
+                        <p class="locationDetailMain">Ben Beo Port, Cat Ba Island, Cat Hai District, Hai Phong City, Vietnam</p>
+                    </div>
+                    <div id="googleMapWrapperDoSon">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59702.2630465683!2d107.00115624176668!3d20.734922595223768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a45b5d29a9b11%3A0x4dd4f928dad786a9!2sLan%20Ha%20Bay!5e0!3m2!1sen!2s!4v1736172204445!5m2!1sen!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" id="googleMapMainDoSon"></iframe>
+                    </div>
+                    <div class="locationTitleWrapper">
+                        <h3 class="locationTitleMain">
+                            Time to travel: 
+                        </h3>
+                        <p class="locationDetailMain">April to June, September to November (Try to avoid July and August because the danger of the rainy and stormy season in northern Vietnam and June for because do not like crowd</p>
+                    </div>
+                    <div class="locationTitleWrapper">
+                        <h3 class="locationTitleMain">
+                            Cuisines:
+                        </h3>
+                    </div>
+                </div>
+                <div id="CuisinesDoSonImageIWrapper">
+                
+                </div>
+                <div class="infoCuisineDoSonWrapper">
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Lan Ha Grilled egg rolls</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba blue crab</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Lan Ha geoduck</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Lan Ha Salted Mantis Shrimp</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Squid dishes</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Lan Ha salt fried crab</h3>
+                    </div>
+                </div>
             </div>
-            <div class="allInfoTravelWrapper">
-
-            </div>
+            <!-- <div class="allInfoTravelWrapper">
+                <div class="titleIntroductionWrapper">
+                    <h2 class="titleFoodMain">
+                        Cat Ba Island
+                    </h2>
+                </div> 
+                <div id="DoSonBeachWrapper">
+                    <div class="locationTitleWrapper">
+                        <h3 class="locationTitleMain">
+                            Location: 
+                        </h3>
+                        <p class="locationDetailMain">Hai Phong city, Vietnam</p>
+                    </div>
+                    <div id="googleMapWrapperDoSon">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29858.147847747812!2d106.77530875644953!3d20.699319529182965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314a6c608b8feabd%3A0x5bf0cbf27e7679c2!2zQsOjaSBiaeG7g24gxJDhu5MgU8ahbg!5e0!3m2!1svi!2s!4v1735183178118!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade" id="googleMapMainDoSon"></iframe >
+                    </div>
+                    <div class="locationTitleWrapper">
+                        <h3 class="locationTitleMain">
+                            Time to travel: 
+                        </h3>
+                        <p class="locationDetailMain">From April to October</p>
+                    </div>
+                    <div class="locationTitleWrapper">
+                        <h3 class="locationTitleMain">
+                            Cuisines:
+                        </h3>
+                    </div>
+                </div>
+                <div id="CuisinesDoSonImageIWrapper">
+                    <div class="OnlyImageWrapper">
+                        <a href="https://www.youtube.com/watch?v=uRUWJXmEhJc"><img src="./image/horseShoeCrab.jpg" class="OnlyImageMain"></a>
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://www.dienmayxanh.com/vao-bep/cach-lam-ca-song-ca-mu-hap-gung-hanh-ngon-ngot-chieu-dai-gia-08854"><img src="./image/CatBaGrouper.jpg" class="OnlyImageMain"></a>
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://chefdzung.com.vn/cach-che-bien-tu-hai-hap-sa.html"><img src="./image/CatBaGeoduck.jpg" class="OnlyImageMain"></a>
+                    </div>  
+                    <div class="OnlyImageWrapper">
+                        <a href="https://haisan.online/be-be-sot-me-139782.html"></a><img src="./image/CatBaMantisShrimp.jpg" class="OnlyImageMain">
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://kingfoodmart.com/bai-viet/2-cach-che-bien-tom-hum-ngon-tuyet-dinh?srsltid=AfmBOopNtXfSZ7nFF87SHxr_wG6_B6vTHzOZcMq02rLp3lizq9gpWmlb"></a><img src="./image/CatBaLobster.jpg" class="OnlyImageMain">
+                    </div>
+                    <div class="OnlyImageWrapper">
+                        <a href="https://www.dienmayxanh.com/vao-bep/cach-lam-bun-tom-hai-phong-chua-chua-ngot-ngot-la-mieng-thom-08299"><img src="./image/vermicelliWithShrimp.jpg" class="OnlyImageMain"></a>
+                    </div>
+                </div>
+                <div class="infoCuisineDoSonWrapper">
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba horseshoe crab</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba grouper</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba geoduck</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba mantis shrimp</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba lobster</h3>
+                    </div>
+                    <div class="infoDoSonWrapper">
+                        <h3 class="infoDoSonMain">Cat Ba vermicelli with shrimp</h3>
+                    </div>
+                </div>
+            </div> -->
+            
             <!-- INFO END -->
         </div>
     </div>
