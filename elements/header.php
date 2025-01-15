e55120895a1661f0a309958577c17dfa7b6d6573
donguyenphu <donguyenphu022008@gmail.com>
2025-01-15 13:48:31 +0700

    update file direction

--- elements/header.php
+++ elements/header.php
@@ -1,3 +1,6 @@
+<?php
+    $curName = basename(__FILE__);
+?>
 <header>
     <!-- illustrated image of Hai Phong -->
     <!-- link section -->
@@ -16,13 +19,13 @@
             </div>
             <div class="allSectionWrapper">
                 <!--  Home,His, cul,travel, cui,peo&mus,con-->
-                <div class="sectionLinkWrapper"><a href="index.php" class="sectionLinkMain">Home</a></div>
-                <div class="sectionLinkWrapper"><a href="history.php" class="sectionLinkMain">History</a></div>
-                <div class="sectionLinkWrapper"><a href="culture.php" class="sectionLinkMain">Culture</a></div>
-                <div class="sectionLinkWrapper"><a href="travel.php" class="sectionLinkMain">Travel</a></div>
-                <div class="sectionLinkWrapper"><a href="cuisine.php" class="sectionLinkMain">Cuisines</a></div>
-                <div class="sectionLinkWrapper"><a href="people.php" class="sectionLinkMain">People and Song</a></div>
-                <div class="sectionLinkWrapper"><a href="contact.php" class="sectionLinkMain">Contact</a></div>
+                <div class="sectionLinkWrapper <?php echo $curName == 'index.php' ? 'active' : '';?>"><a href="index.php" class="sectionLinkMain">Home</a></div>
+                <div class="sectionLinkWrapper <?php echo $curName == 'history.php' ? 'active' : '';?>"><a href="history.php" class="sectionLinkMain">History</a></div>
+                <div class="sectionLinkWrapper <?php echo $curName == 'culture.php' ? 'active' : '';?>"><a href="culture.php" class="sectionLinkMain">Culture</a></div>
+                <div class="sectionLinkWrapper <?php echo $curName == 'travel.php' ? 'active' : '';?>"><a href="travel.php" class="sectionLinkMain">Travel</a></div>
+                <div class="sectionLinkWrapper <?php echo $curName == 'cuisine.php' ? 'active' : '';?>"><a href="cuisine.php" class="sectionLinkMain">Cuisines</a></div>
+                <div class="sectionLinkWrapper <?php echo $curName == 'people.php' ? 'active' : '';?>"><a href="people.php" class="sectionLinkMain">People and Song</a></div>
+                <div class="sectionLinkWrapper <?php echo $curName == 'contact.php' ? 'active' : '';?>"><a href="contact.php" class="sectionLinkMain">Contact</a></div>
             </div>
         </div>
     </div>
