e941e1f6a29af0c72ded5e7a024083f11486d1a2
donguyenphu <donguyenphu022008@gmail.com>
2025-05-12 01:31:35 +0700

    update history Information

--- history.php
+++ history.php
@@ -38,47 +38,81 @@ $level = './';
                 </div>
             </div>
             <!-- //////// -->
-            <div class="infoDoSonWrapper" style="align-items: center; display : flex; margin-bottom: 15px; cursor: pointer; gap: 30px;">
+            <div class="infoDoSonWrapper" style="align-items: center; display : flex; margin-bottom: 15px; gap: 30px;">
                 <!-- ignore hover effects -->
                 <h3 class="Events" style="pointer-events: none;">Ancient period</h3>
-                <button class="buttonHide" style="cursor: pointer; padding: 15px; border-radius: 15px; font-size: 30px; font-weight: 600;">Hide</button>
+                <!-- <button class="buttonHide" style="cursor: pointer; padding: 10px; border-radius: 15px; font-size: 25px; font-weight: 600;">Hide</button> -->
             </div>
             <div class="allInfoTravelWrapper history">
-                <div class="imageAndContentCuisineWrapper">
+                <div class="imageAndContentCuisineWrapper" style="gap: 30px;">
                     <div class="imgCNSwrapper">
-                        <img src="image/Banh_my_que.jpg" class="imgCNSmain">
+                        <img src="./image/nhung-su-that-khong-ngo-ve-thoi-an-long-o-lo.webp" class="imgCNSmain">
                     </div>
                     <div class="foodDetailWrapper">
                         <p class="foodDetailMain cuisine">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, similique!
+                            - According to archaeology , Hai Phong was once inhabited by ancient Vietnamese people about 3,000 - 4,000 years ago.
+                        </p>
+                        <p class="foodDetailMain cuisine">
+                            - There are 2 typical monuments of Hai Phong City in the Ancient period: 
+                            <p class="foodDetailMain cuisine">
+                                + Cai Beo relic site: dating back about 4000 BC, it belongs to Ha Long's culture, a typical culture of prehistoric coastal residents.
+                            </p>
+                            <p class="foodDetailMain cuisine">
+                                + Trang Kenh relic site: Related to Dong Son's culture (about 700 BC - 100 AD), notable for bronze drums and weapons, it proves the developed civilization of residents of the residents.
+                            </p>
                         </p>
                     </div>
                 </div>
             </div>
-            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; cursor: pointer; gap: 30px;">
+            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; gap: 30px;">
                 <h3 class="Events" style="pointer-events: none;">Feudal Period</h3>
-                <button class="buttonHide" style="cursor: pointer; padding: 15px; border-radius: 15px; font-size: 30px; font-weight: 600;">Hide</button>
+                <!-- <button class="buttonHide" style="cursor: pointer; padding: 10px; border-radius: 15px; font-size: 25px; font-weight: 600;">Hide</button> -->
             </div>
             <div class="allInfoTravelWrapper history">
-                <div class="imageAndContentCuisineWrapper">
+                <div class="imageAndContentCuisineWrapper" style="gap: 30px;">
                     <div class="imgCNSwrapper">
-                        <img src="image/Banh_my_que.jpg" class="imgCNSmain">
+                        <img src="./image/redsvn-hai-phong-xua-04.jpg" class="imgCNSmain">
                     </div>
                     <div class="foodDetailWrapper">
+                        <strong class="foodDetailMain cuisine">1. Ly - Tran Dynasty (11st - 14th Century)</strong>
                         <p class="foodDetailMain cuisine">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, similique!
+                            - At that time, Hai Phong hadn't appeared that time. However, locations like An Duong, An Hai, Thuy Nguyen were already lied in states of Dai Viet. The city lied in Hai Dong route, a large administrative unit in Northern Coastal.
+                        </p>
+                        <strong class="foodDetailMain cuisine">2. Ho - Le So Dynasty (15th Century)</strong>
+                        <p class="foodDetailMain cuisine">
+                            - In 1407, Hai Phong city was destructed badly by the Minh army
+                        </p>
+                        <p class="foodDetailMain cuisine">
+                            - After 1428, Hai Phong lied in Hai Duong town, continuing to be consolidated in administration and algricultural development.
+                        </p>
+                        <strong class="foodDetailMain cuisine">3. Le Trung Hung - Trinh - Nguyen (16th - 18th Century)</strong>
+                        <p class="foodDetailMain cuisine">
+                            - Hai Phong located in Kinh Mon district, Hai Duong town.
+                        </p>
+                        <p class="foodDetailMain cuisine">
+                            - Trading, boat building, salt making and fishing was popular and developed. In this period, many conventional craft villages appeared, such as bronze casting, pottery making, mat weaving, etc.
+                        </p>
+                        <strong class="foodDetailMain cuisine">4. End of feudal period - Nguyen Dynasty (19th Century)</strong>
+                        <p class="foodDetailMain cuisine">
+                            - In 1804, Nguyen Dynasty established Hai An town, consisting of coastal districts of Hai Duong, including most of Hai Phong.
+                        </p>
+                        <p class="foodDetailMain cuisine">
+                            - In the mid-19th century , this land attracted the attention of French colonialists because of its strategic location - a water traffic hub, leading to the Gulf of Tonkin.
+                        </p>
+                        <p class="foodDetailMain cuisine">
+                            - In 1873, France invaded Tonkin. In 1888, Hai Phong was officially established as a port city under French control – ending the feudal period and opening the colonial period.
                         </p>
                     </div>
                 </div>
             </div>
-            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; cursor: pointer; gap: 30px;">
+            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; gap: 30px;">
                 <h3 class="Events" style="pointer-events: none;">French Colonial Period (1888–1945)</h3>
-                <button class="buttonHide" style="cursor: pointer; padding: 15px; border-radius: 15px; font-size: 30px; font-weight: 600;">Hide</button>
+                <!-- <button class="buttonHide" style="cursor: pointer; padding: 10px; border-radius: 15px; font-size: 25px; font-weight: 600;">Hide</button> -->
             </div>
             <div class="allInfoTravelWrapper history">
-                <div class="imageAndContentCuisineWrapper">
+                <div class="imageAndContentCuisineWrapper" style="gap: 30px;">
                     <div class="imgCNSwrapper">
-                        <img src="image/Banh_my_que.jpg" class="imgCNSmain">
+                        <img src="./image/Dien-Bien-Phu.jpg" class="imgCNSmain">
                     </div>
                     <div class="foodDetailWrapper">
                         <p class="foodDetailMain cuisine">
@@ -87,14 +121,14 @@ $level = './';
                     </div>
                 </div>
             </div>
-            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; cursor: pointer; gap: 30px;">
+            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; gap: 30px;">
                 <h3 class="Events" style="pointer-events: none;">Resistance Period (1945–1975)</h3>
-                <button class="buttonHide" style="cursor: pointer; padding: 15px; border-radius: 15px; font-size: 30px; font-weight: 600;">Hide</button>
+                <!-- <button class="buttonHide" style="cursor: pointer; padding: 10px; border-radius: 15px; font-size: 25px; font-weight: 600;">Hide</button> -->
             </div>
             <div class="allInfoTravelWrapper history">
-                <div class="imageAndContentCuisineWrapper">
+                <div class="imageAndContentCuisineWrapper" style="gap: 30px;">
                     <div class="imgCNSwrapper">
-                        <img src="image/Banh_my_que.jpg" class="imgCNSmain">
+                        <img src="./image/Cuoc-dau-tranh-300-ngay-giai-phong.png" class="imgCNSmain">
                     </div>
                     <div class="foodDetailWrapper">
                         <p class="foodDetailMain cuisine">
@@ -103,14 +137,14 @@ $level = './';
                     </div>
                 </div>
             </div>
-            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; cursor: pointer; gap: 30px;">
+            <div class="infoDoSonWrapper" style="align-items: center; display: flex; margin-bottom: 15px; gap: 30px;">
                 <h3 class="Events" style="pointer-events: none;">Modern Period (1975–present)</h3>
-                <button class="buttonHide" style="cursor: pointer; padding: 15px; border-radius: 15px; font-size: 30px; font-weight: 600;">Hide</button>
+                <!-- <button class="buttonHide" style="cursor: pointer; padding: 10px; border-radius: 15px; font-size: 25px; font-weight: 600;">Hide</button> -->
             </div>
             <div class="allInfoTravelWrapper history">
-                <div class="imageAndContentCuisineWrapper">
+                <div class="imageAndContentCuisineWrapper" style="gap: 30px;">
                     <div class="imgCNSwrapper">
-                        <img src="image/Banh_my_que.jpg" class="imgCNSmain">
+                        <img src="./image/Hai-phong-modern-period.jpg" class="imgCNSmain">
                     </div>
                     <div class="foodDetailWrapper">
                         <p class="foodDetailMain cuisine">
