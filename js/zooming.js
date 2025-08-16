7fbe263bcf66a83255291b852047ed3f0f1cdd7d
donguyenphu <donguyenphu022008@gmail.com>
2025-08-16 13:27:33 +0700

    update zooming.js

--- js/zooming.js
+++ js/zooming.js
@@ -1,24 +1,14 @@
-// can zoom 75 <= current <= 110
+// can zoom 80 <= current <= 100
 window.addEventListener('wheel', function (e) {
     const zoomLevel = Math.round(window.devicePixelRatio * 100);
     console.log(zoomLevel);
     if (e.ctrlKey) {
-        if (e.deltaY < 0 && zoomLevel > 100) { // up
-            e.preventDefault();
-        }
-        else if (e.deltaY > 0 && zoomLevel < 80) { // down
-            e.preventDefault();
-        }
+        e.preventDefault();
     }
 }, { passive: false });
 window.addEventListener('keydown', function (e) {
     const zoomLevel = Math.round(window.devicePixelRatio * 100);
-    if (e.ctrlKey || e.metaKey) {
-        if ((e.key === '+' || e.key === '=') && zoomLevel > 100) {
-            e.preventDefault();
-        }
-        else if ((e.key === '-') && zoomLevel < 80) {
-            e.preventDefault();
-        }
+    if ((e.ctrlKey || e.metaKey) && (e.key === '+' || e.key === '=' || e.key === '0' || e.key === '-')) {
+        e.preventDefault();
     }
 });
